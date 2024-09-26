43921337d31c3053b75ecd695d5f5270ae2d1d5a
Joao Stephan <joaossmauricio@gmail.com>
2024-09-26 20:07:32 -0300

    vitor olha isso

--- problem.py
+++ problem.py
@@ -1,5 +1,5 @@
 import numpy as np
-from mealpy.utils.problem import FloatVar
+from mealpy.utils.problem import FloatVar, IntegerVar
 
 D = 160
 d = 90
@@ -66,11 +66,12 @@ def evaluate(solution):
 
     T = D - d - 2 * D_b
     x = (
-        ((D - d) / 2 - 3 * T / 4) ** 2
-        + (D / 2 - T / 4 / D_b) ** 2
+        ((D - d) / 2 - 3 * (T / 4)) ** 2
+        + (D / 2 - T / 4 - D_b) ** 2
         - (d / 2 + T / 4) ** 2
     )
-    y = 2 * ((D - d) / 2 - 3 * T / 4) * (D / 2 - T / 4 - D_b)
+    y = 2 * ((D - d) / 2 - 3 * (T / 4)) * (D / 2 - T / 4 - D_b)
+    print(x / y)
     phi_0 = 2 * np.pi - np.arccos(x / y)
     gamma = D_b / D_m
     f_i = r_i / D_b
@@ -82,33 +83,42 @@ def evaluate(solution):
         f = 3.647 * f_c(gamma, f_i, f_o) * Z ** (2 / 3) * D_b ** (1.4)
 
     penalty = 0
-    increase = -1e6
+    increase = 0
 
     if not g1(phi_0, Z, D_b, D_m):
         penalty += increase
+        print("g1")
     if not g2(D_b, K_D_min, D, d):
         penalty += increase
+        print("g2")
     if not g3(K_D_max, D, d, D_b):
         penalty += increase
+        print("g3")
     if not g4(zeta, B_w, D_b):
         penalty += increase
+        print("g4")
     if not g5(D_m, D, d):
         penalty += increase
+        print("g5")
     if not g6(epsilon, D, d, D_m):
         penalty += increase
+        print("g6")
     if not g7(D, D_m, D_b, Epsilon):
         penalty += increase
+        print("g7")
     if not g8(f_i):
         penalty += increase
+        print("g8")
     if not g9(f_o):
         penalty += increase
+        print("g9")
 
     return f + penalty
 
 
 D_m_bounds = FloatVar(0.5 * (D + d), 0.6 * (D + d))
 D_b_bounds = FloatVar(0.15 * (D - d), 0.45 * (D - d))
-Z_bounds = FloatVar(4, 50)
+Z_bounds = IntegerVar(4, 50)
 f_i_bounds = FloatVar(0.515, 0.6)
 f_o_bounds = FloatVar(0.515, 0.6)
 K_D_min_bounds = FloatVar(0.4, 0.5)
@@ -129,3 +139,19 @@ bounds = [
     epsilon_bounds,
     zeta_bounds,
 ]
+
+if __name__ == "__main__":
+    solution = [
+        125.7171,
+        21.423,
+        11,
+        0.515,
+        0.515,
+        0.4159,
+        0.651,
+        0.300043,
+        0.0223,
+        0.751,
+    ]
+
+    print(evaluate(solution))
